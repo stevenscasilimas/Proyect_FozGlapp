a735c8a2a3aa6cdca1f93c7f07464ec478979727
Stevens Casilimas <pablostevens1990gmail.com>
2018-08-10 21:47:37 -0500

    Cuenta de usuarios y administrador usando la gema devise

--- Gemfile
+++ Gemfile
@@ -9,6 +9,7 @@ gem 'rails', '~> 5.2.1'
 # gem 'sqlite3'
 gem 'pg'
 gem 'mail_form'
+gem 'devise'
 # Use Puma as the app server
 gem 'puma', '~> 3.11'
 # Use SCSS for stylesheets
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
+  devise_for :users
   resources :contacts, only: [:new, :create]
-  
-  
+  get 'home/index'
   #get 'contacts/new'
   #get 'contacts/create'
-root to: 'home#index'
-  get 'home/index'
+  root to: 'home#index'
+  #get 'home/index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
